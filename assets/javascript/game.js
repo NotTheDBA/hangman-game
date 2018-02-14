baa2fc4170b8137b3989ace4edd9bffe6abf82d7
RSReitz <notthedba@outlook.com>
2018-02-14 16:24:37 -0500

    Added back modified handler for tracking wrong guesses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,53 @@
 
 //  1) create list of words
 var wordlist = ["rock", "paper", "scissors"];
-var guesses = 13;
-var wrongGuesses = [];
 
+// 2) randomly select first word (array of characters?)
 //TODO Build magic word and player placeholder word dynamically...
 var magicWord = ["R", "O", "C", "K"];
+
+// 3) build masked string of proper length for the word
 var playerWord = ["-", "-", "-", "-"];
+
+// 4) initialize guesses to 13
+var guesses = 13;
+
+var wrongGuesses = [];
 var judge;
 
 // 5) create onkey listener
 document.onkeyup = function (event) {
 
-    // 2) randomly select first word (array of characters?)
-    // 3) build masked string of proper length for the word
-    // 4) initialize guesses to 13
-
     // 6) when player presses key
     // Determines which key was pressed.
-    var playerGuess = event.key.toUpperCase();
+    var currentGuess = event.key.toUpperCase();
 
     // - ignore non-alphabet - ascii (65-90)
-    if (playerGuess.charCodeAt(0) >= 65 && playerGuess.charCodeAt(0) <= 90) {
-        var ltrPos = magicWord.indexOf(playerGuess);
-        if (ltrPos >= 0) {
-            playerWord[ltrPos] = magicWord[ltrPos];
-            judge = "";
-            playerWord.forEach(function (element) {
-                judge += element + " ";
-            });
-            document.querySelector("#hidden-word").innerHTML = judge;
-        }
-        // // - ignore previous guesses
-        // if (guesses.indexOf(playerGuess) < 0) {
-        //     // - reduce guess count
-        //     // - if in word, reveal letter in word 
-        //     // - if not in word, add to guesses
-        //     wrongGuesses.push(playerGuess);
-        //     document.querySelector("#guesses").innerHTML += playerGuess + " ";
-        //     // - if guess count is 0, end game
-        //     document.querySelector("#guesses-left").innerHTML = guesses;
-        // }
-    }
+    if (currentGuess.charCodeAt(0) >= 65 && currentGuess.charCodeAt(0) <= 90) {
 
-    // - if word revealed, add to wins
-    // 7) reset and select new word    
+        // // - ignore previous guesses
+        if (playerWord.indexOf(currentGuess) < 0) {
+
+            var ltrPos = magicWord.indexOf(currentGuess);
+
+            if (ltrPos >= 0) {
+                // - if in word, reveal letter in word 
+                playerWord[ltrPos] = magicWord[ltrPos];
+                judge = "";
+                playerWord.forEach(function (element) {
+                    judge += element + " ";
+                });
+                document.querySelector("#hidden-word").innerHTML = judge;
+            } else if (wrongGuesses.indexOf(currentGuess) < 0) {
+                // - if not in word, add to guesses
+                wrongGuesses.push(currentGuess);
+                document.querySelector("#guesses").innerHTML += currentGuess + " ";
+                guesses--;
+                document.querySelector("#guesses-left").innerHTML = guesses;
+            }
+            // - if guess count is 0, end game
+            // - if word revealed, add to wins
+            // 7) reset and select new word    
+        }
+    }
 }
