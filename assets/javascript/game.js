7ace8997f2de21463a625cebe1539eca22828ec6
RSReitz <notthedba@outlook.com>
2018-02-15 10:47:59 -0500

    Gameplay logic complete.  Core methods extracted for clarity.  Added TODO items.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,32 @@
-
+//TODO: Capture larger list of words
 //  1) create list of words
 var wordlist = ["rock", "paper", "scissors"];
 
+//TODO: Build magic word and player placeholder word dynamically...
 // 2) randomly select first word (array of characters?)
-//TODO Build magic word and player placeholder word dynamically...
 var magicWord = ["R", "O", "C", "K"];
 
-// 3) build masked string of proper length for the word
-var playerWord = ["-", "-", "-", "-"];
+var winCount = 0;
+var gameover = true;
 
-// 4) initialize guesses to 13
-var guesses = 13;
-var gameover = false;
+//These are initialized in the setupGame function, called at game start...
+var guesses, playerWord, wrongGuesses;
 
-var wrongGuesses = [];
-var judge;
+promptToPlay("Press any key to begin...");
 
 // 5) create onkey listener
 document.onkeyup = function (event) {
 
     // 6) when player presses key
-    // Determines which key was pressed.
     var currentGuess = event.key.toUpperCase();
-    console.log(currentGuess + " | " + currentGuess.length + " | " + currentGuess.charCodeAt(0));
+
+    // Determines which key was pressed.
     if (gameover) {
-        //wait for space bar to restart           
-        //if (currentGuess = "space bar") {
-        //     7) reset and select new word    
-        // }
+        //wait for any key to start
+        setUpGame();
     } else {
         //  - ignore non-alphabet - ascii (65-90) 
-        if (currentGuess.length = 1 && currentGuess.charCodeAt(0) >= 65 && currentGuess.charCodeAt(0) <= 90) {
+        if (currentGuess.length === 1 && currentGuess.charCodeAt(0) >= 65 && currentGuess.charCodeAt(0) <= 90) {
 
             // // - ignore previous guesses
             if ((playerWord.indexOf(currentGuess) < 0) && (wrongGuesses.indexOf(currentGuess) < 0)) {
@@ -40,37 +36,81 @@ document.onkeyup = function (event) {
                 if (ltrPos >= 0) {
                     // - if in word, reveal letter in word 
                     playerWord[ltrPos] = magicWord[ltrPos];
-                    judge = "";
-                    playerWord.forEach(function (element) {
-                        judge += element + " ";
-                    });
-                    document.querySelector("#hidden-word").innerHTML = judge;
+                    displayGuess(playerWord);
+
                 } else {
                     // - if not in word, add to guesses
-                    wrongGuesses.push(currentGuess);
-                    document.querySelector("#guesses").innerHTML += currentGuess + " ";
-                    guesses--;
-                    document.querySelector("#guesses-left").innerHTML = guesses;
+                    addToGuesses(currentGuess);
                 }
 
-                // - if guess count is 0, end game
+                // check if end of game
+                // - if guess count is 0, 
                 if (guesses === 0) {
                     //Declare loser
-
+                    declareFinal("Sorry, you lose!")
                 }
                 // - if word revealed, add to wins
                 if (playerWord.indexOf("-") < 0) {
-                    //Declare winner
                     //increment wins
+                    incrementWins();
+
+                    //Declare winner
+                    declareFinal("WINNER!")
+
                 }
 
-
-
             } else {
-                document.querySelector("#game-alert").innerHTML = "Guess again...";
+                promptToPlay("Guess again...");
             }
         } else {
-            document.querySelector("#game-alert").innerHTML = "Press any letter to play...";
+            promptToPlay("Press any letter to play...");
         }
     }
+}
+
+function incrementWins() {
+    winCount++;
+    document.querySelector("#win-count").innerHTML = winCount;
+}
+
+function declareFinal(prompt) {
+    prompt += "<br>Press any key to play again."
+    gameover = true;
+    promptToPlay(prompt);
+}
+
+function addToGuesses(currentGuess) {
+    wrongGuesses.push(currentGuess);
+    document.querySelector("#guesses").innerHTML += currentGuess + " ";
+    guesses--;
+    document.querySelector("#guesses-left").innerHTML = guesses;
+}
+
+function displayGuess(maskedWord) {
+    var judge = "";
+    maskedWord.forEach(function (element) {
+        judge += element + " ";
+    });
+    document.querySelector("#hidden-word").innerHTML = judge;
+}
+
+function setPlayerWord(word) {
+    // TODO: actually build proper length array
+    // 3) build masked string of proper length for the word
+    return ["-", "-", "-", "-"]
+}
+
+function setUpGame() {
+
+    gameover = false;
+    guesses = 13;
+    wrongGuesses = [];
+    document.querySelector("#guesses").innerHTML = "";
+    playerWord = setPlayerWord("rock");
+    displayGuess(playerWord);
+    promptToPlay("Press any letter to play...");
+}
+
+function promptToPlay(message) {
+    document.querySelector("#game-alert").innerHTML = message;
 }
